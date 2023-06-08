9f66a424f19d3c39e9783280cabeaaa7c52bba54
fiqgant <fiqgant@gmail.com>
2023-06-08 13:56:07 +0700

    week 9

--- ReactJS/week9/src/App.js
+++ ReactJS/week9/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './App.css';
 import TodoList from './components/TodoList';
 import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -20,10 +21,16 @@ function App() {
                 Tentang
               </NavLink>
             </li>
+            <li>
+              <NavLink to="/contact" activeClassName="active">
+                Contact
+              </NavLink>
+            </li>
           </ul>
         </nav>
 
         <Route exact path="/" component={TodoList} />
+        <Route exact path="/contact" component={Contact} />
         <Route path="/about" component={About} />
       </div>
     </Router>
